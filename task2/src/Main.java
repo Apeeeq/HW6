09f865d41a737ea17d4ca1b0d52f90dab4a188a9
Apeeeq <gasarden@yandex.ru>
2025-03-20 12:47:49 +0300

    Добавлен тернальный оператор

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -13,35 +13,38 @@ public class Main {
         System.out.println("Введите третий вес: ");
         double third = new Scanner(System.in).nextDouble();
 
-        double greatest = 0;
-        double average = 0;
-        double smallest = 0;
-        //todo дописать логику программы ниже.
 
-        if (first > second && first > third) {
-            greatest = first;
-        } else if (first < second && first < third) {
-            smallest = first;
-        } else {
-            average = first;
-        }
+// Тернальные операторы
+        double greatest = (first > second && first > third ? first : first < second && third < second ? second : third);
+        double average = (first < second && first > third ? first : second < first && second > third ? second : third);
+        double smallest = (first < second && first < third ? first : second < first && second < third ? second : third);
 
-        if (second > first && second > third) {
-            greatest = second;
-        } else if (second < first && second < third) {
-            smallest = second;
-        } else {
-            average = second;
-        }
+//        if (first > second && first > third) {
+//            greatest = first;
+//        } else if (first < second && first < third) {
+//            smallest = first;
+//        } else {
+//            average = first;
+//        }
+//
+//        if (second > first && second > third) {
+//            greatest = second;
+//        } else if (second < first && second < third) {
+//            smallest = second;
+//        } else {
+//            average = second;
+//        }
+//
+//        if (third > second && third > first) {
+//            greatest = third;
+//        } else if (third < second && third < first) {
+//            smallest = third;
+//        } else {
+//            average = third;
+//
+//        }
 
-        if (third > second && third > first) {
-            greatest = third;
-        } else if (third < second && third < first) {
-            smallest = third;
-        } else {
-            average = third;
 
-        }
         System.out.println(greatest);
         System.out.println(average);
         System.out.println(smallest);
